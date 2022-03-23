180d5e8e25ae7cbbb1ee44c15cacdb3220081a47
María Naranjo Almeida <maria2000naranjo@gmail.com>
2022-03-23 15:27:18 +0000

    Edited dataManagement.js

--- dataManagement.js
+++ dataManagement.js
@@ -1,8 +1,14 @@
-const routeActivities = require('./routeActivities.json');
+$(document).ready(function(){
+    $.getJSON('predefinedRoutes.json', function(json) {
+        for(var i = 0; i < json.routeName; i++){
+            console.log(i);
+        }
+    })
+    /*function getRouteActivities() {
+        for (let i = 0; i < routeActivities.length; i++) {
+            console.log(`id: ${routeActivities[i].activityId}, name:${routeActivities[i].activityName}`)
+        }
+    }*/
+});
 
 
-function getRouteActivities() {
-    for (let i = 0; i < routeActivities.length; i++) {
-        console.log(`id: ${routeActivities[i].activityId}, name:${routeActivities[i].activityName}`)
-    }
-}
